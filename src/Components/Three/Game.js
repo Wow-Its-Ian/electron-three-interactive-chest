00bf8d43a931f4c7e7c8549b1b5f68d514dcc31b
Wow-Its-Ian <wallsianh@gmail.com>
2022-02-10 15:20:28 -0500

    added chest open animation

--- src/Components/Three/Game.js
+++ src/Components/Three/Game.js
@@ -1,43 +1,22 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Canvas } from 'react-three-fiber';
-import { OrbitControls, Stars } from '@react-three/drei';
+import { OrbitControls, softShadows } from '@react-three/drei';
 import { Physics, useBox, usePlane } from '@react-three/cannon';
 import ChestModel from './GLTF/Chest';
 import Lights from './Lights';
 import Floor from './Floor';
 
-// function Box() {
-//   const [boxRef, boxApi] = useBox(() => ({ mass: 1 }));
-//   return (
-//     <mesh
-//       onClick={() => {
-//         boxApi.velocity.set(0, 5, 0);
-//       }}
-//       ref={boxRef}
-//       position={[0, 2, 0]}
-//     >
-//       <boxBufferGeometry attach="geometry" />
-//       <meshLambertMaterial attach="material" color="hotpink" />
-//     </mesh>
-//   );
-// }
-
-// function Ground() {
-//   const [groundRef] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
-//   return (
-//     <mesh ref={groundRef} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-//       <planeBufferGeometry attach="geometry" args={[100, 100]} />
-//       <meshLambertMaterial attach="material" color="lightblue" />
-//     </mesh>
-//   );
-// }
+// soften shadows with drei softShadows();
+softShadows();
 
 export default function Game() {
+  const [chestOpen, setChestOpen] = useState(false);
+
   return (
     <Canvas colorManagement shadows camera={{ position: [-5, 4, 4], fov: 40 }}>
       <Lights />
       <Suspense fallback={null}>
-        <ChestModel />
+        <ChestModel open={chestOpen} setOpen={setChestOpen} />
         <Floor />
       </Suspense>
       {/* <color attach="background" args={['black']} /> */}
--- src/Components/Three/Lights.js
+++ src/Components/Three/Lights.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Lights = () => {
   return (
     <>
-      <fog attach="fog" args={['fff', 0, 22]} />
+      <fog attach="fog" args={['#fff', 0, 22]} />
       <ambientLight intensity={0.4} />
       <directionalLight
         castShadow
